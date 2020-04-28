605ae06212cd2f479cac9e9c03b33c66031c623d
SuperDroidBond <superdroidbond@yahoo.com>
2020-04-28 19:50:18 +0530

    xtras: move our themes to xtras
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- ColtSpares.mk
+++ ColtSpares.mk
@@ -36,3 +36,6 @@ include vendor/ColtSpares/prebuilts/prebuilts.mk
 
 # Includes qsthemes
 include vendor/ColtSpares/qsthemes/qsthemes.mk
+
+# Includes themes
+include vendor/ColtSpares/themes/themes.mk
