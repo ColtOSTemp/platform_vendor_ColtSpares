2ebdc63604af064043b8103981d4618cd975c043
pavanpaps <pavanishot200@gmail.com>
2021-02-08 15:00:47 +0000

    ColtSpares: Add Gesture bar radius [3/3]
    Reverse engineered from G-visual Mod from @ Gnonymous7
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- navbar/navbar.mk
+++ navbar/navbar.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2020-21 The Project-Xtended
+# Copyright (C) 2021 The ColtOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,4 +16,7 @@
 PRODUCT_PACKAGES += \
     GesturalNavigationOverlayLong \
     GesturalNavigationOverlayMedium \
-    GesturalNavigationOverlayHidden
+    GesturalNavigationOverlayHidden \
+    GesturalNavigationRadiusLow \
+    GesturalNavigationRadiusVeryLow \
+    GesturalNavigationRadiusHidden
