714171571080f96b4a1c262c299274549a4389ab
Sonal Singh <sonal.singh.19993@gmail.com>
2021-04-28 17:50:34 +0530

    ColtSpares: Move all accents overlays to vendor/ColtSpares [2/2]
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- accents/AccentColorAmethystOverlay/Android.mk
+++ accents/AccentColorAmethystOverlay/Android.mk
@@ -1,26 +1,29 @@
-# Copyright (C) 2021 The ColtOS Project
+#
+#  Copyright (C) 2020, The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
 
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+LOCAL_RRO_THEME := AccentColorAmethyst
 
-# Includes accents
-include vendor/ColtSpares/accents/accents.mk
+LOCAL_PRODUCT_MODULE := true
 
-# Includes icon packs/shapes
-include vendor/ColtSpares/icons/icons.mk
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-# Includes prebuilts
-include vendor/ColtSpares/prebuilts/prebuilts.mk
+LOCAL_PACKAGE_NAME := AccentColorAmethystOverlay
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_RRO_PACKAGE)
