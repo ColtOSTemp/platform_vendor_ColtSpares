5309543c454382477fd805282ad3bf83aa6a78fc
Mrinal Ghosh <mg712702@gmail.com>
2021-05-12 14:41:08 +0530

    Add our prebuilt apps
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

---
+++ prebuilts/prebuilts.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+# Prebuilt packages
+PRODUCT_PACKAGES += \
+    MetroMusicPlayer \
+    WallpaperPickerGoogleRelease
+
+PRODUCT_COPY_FILES += \
+    vendor/ColtSpares/prebuilts/etc/permissions/privapp-permissions-prebuilt-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-prebuilt.xml
