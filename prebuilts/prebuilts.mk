22ab40ef967e0cd2a7f9dfd89ae54038f1ef226e
Mrinal Ghosh <mg712702@gmail.com>
2021-06-26 14:30:54 +0530

    ColtSpares: Update prebuilts
    * Remove SimpleGallery & GoogleWallpaperPicker
    * Add GalleryGo
    
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- prebuilts/prebuilts.mk
+++ prebuilts/prebuilts.mk
@@ -4,8 +4,8 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 # Prebuilt packages
 PRODUCT_PACKAGES += \
-    MetroMusicPlayer \
-    WallpaperPickerGoogleRelease
+    GalleryGoPrebuilt \
+    MetroMusicPlayer
 
 PRODUCT_COPY_FILES += \
     vendor/ColtSpares/prebuilts/etc/permissions/privapp-permissions-prebuilt-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-prebuilt.xml
