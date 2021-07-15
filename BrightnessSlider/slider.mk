7392fc0d941af54c0d6357a84072ae0f0c753924
spkal01 <kalligeross@gmail.com>
2021-07-15 23:40:52 +0700

    Introduce A12 Brightness Slider [3/3]
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- BrightnessSlider/slider.mk
+++ BrightnessSlider/slider.mk
@@ -4,4 +4,5 @@ PRODUCT_PACKAGES += \
     BrightnessSliderMemeMini \
     BrightnessSliderMemeRound \
     BrightnessSliderMemeRoundStroke \
-    BrightnessSliderMemeStroke
+    BrightnessSliderMemeStroke \
+    BrightnessSliderA12
